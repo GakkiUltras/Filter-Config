a1aa76f7808cae04d4f1c8a398368ffde51c04d9
GakkiUltras <69998409+GakkiUltras@users.noreply.github.com>
2020-09-10 18:36:52 +0800

    edit jquery
    make it creat table dynamically

--- FilterConfig/FilterConfig.js
+++ FilterConfig/FilterConfig.js
@@ -1,28 +1,49 @@
+var filter;
 $(document).ready(function () {
   $("#open-filter").click(function () {
     $.getJSON("./FilterConfig.json", function (data) {
       // $("#product-name-list>p").html(data.ProductName);
       // $("#version-list>p").html(data.ProdcutVersion);
       // $("#keywords>p").html(data.CustomizedKeywords[0]);
-      if (data.hasOwnProperty("Category1")) {
-        $(".category1-checkbox").prop("checked", true);
-        if (data.Category1.hasOwnProperty("Rule1")) {
-          $(".category1-rule1-checkbox").prop("checked", true);
-        }
-        if (data.Category1.hasOwnProperty("Rule2")) {
-          $(".category1-rule2-checkbox").prop("checked", true);
-        }
-      }
-      if (data.hasOwnProperty("Category2")) {
-        $(".category2-checkbox").prop("checked", true);
-        if (data.Category2.hasOwnProperty("Rule1")) {
-          $(".category2-rule1-checkbox").prop("checked", true);
-        }
-        if (data.Category2.hasOwnProperty("Rule2")) {
-          $(".category2-rule2-checkbox").prop("checked", true);
+      for (var i = 0; i < data.Category.length; i++) {
+        for (var j = 0; j < data.Category[i].Rule.length; j++) {
+          var tr = $("<tr></tr>");
+          var td0 = $('<td class="text-center"></td>').append('<input class="category'+i+'-checkbox" type="checkbox">');
+          var td1 = $("<td></td>").text(data.Category[i].Name);
+          var td2 = $('<td class="text-center"></td>').append('<input class="category'+i+'-rule-checkbox" type="checkbox">');
+          var td3 = $("<td></td>").text(data.Category[i].Rule[j].Name);
+          var td4 = $("<td></td>").text(data.Category[i].Rule[j].Description);
+          tr.append(td0, td1, td2, td3, td4);
+          $("tbody").append(tr);
         }
       }
+      filter = data;
+      // if (data.hasOwnProperty("Category1")) {
+      //   $(".category1-checkbox").prop("checked", true);
+      //   if (data.Category1.hasOwnProperty("Rule1")) {
+      //     $(".category1-rule1-checkbox").prop("checked", true);
+      //   }
+      //   if (data.Category1.hasOwnProperty("Rule2")) {
+      //     $(".category1-rule2-checkbox").prop("checked", true);
+      //   }
+      // }
+      // if (data.hasOwnProperty("Category2")) {
+      //   $(".category2-checkbox").prop("checked", true);
+      //   if (data.Category2.hasOwnProperty("Rule1")) {
+      //     $(".category2-rule1-checkbox").prop("checked", true);
+      //   }
+      //   if (data.Category2.hasOwnProperty("Rule2")) {
+      //     $(".category2-rule2-checkbox").prop("checked", true);
+      //   }
+      // }
     });
+  });
+  $(".category-checkbox").click(function () {
+    if ($(this).prop("checked")){
+      var class_name=$(this).attr("class");
+      $('[class='+class_name+']').prop("checked", true);
+      $('[class='+class_name+']')
+    }
   });
   $(".category1-checkbox").click(function () {
     if ($(this).prop("checked")) {
